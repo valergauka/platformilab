b5ce86a916bc6b14c864f581c7712aad87dc29e2
Lera Gauka <vgauka00@gmail.com>
2022-11-14 16:40:05 +0200

    Добавьте файлы проекта.

---
+++ phoneConversations/phoneCcnversations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phoneCcnversations
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaulConnection"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString.ToString());
+            await conn.OpenAsync();
+            SqlCommand cmd = new SqlCommand("select abonents.surName as abonentSurName, abonents.lastName as abonentLastName," +
+                "abonents.fisrtName as abonentFisrtName, cities.name as cityName," +
+                "dateConversation,duration from conversations inner join abonents on conversations.abonentId=abonents.id " +
+                "inner join cities on conversations.cityId=cities.id", conn);
+            SqlDataReader dr = await cmd.ExecuteReaderAsync();
+            Console.WriteLine("{0,-35} {1,-15} {2,-10} {3,-20}\n", "Abonent", "City", "Duration", "Date conversation");
+            while (dr.Read())
+            {
+                string abonentSurName = dr["abonentSurName"].ToString() + " " + dr["abonentLastName"].ToString() + " " + dr["abonentFisrtName"].ToString();
+                string cityName = dr["cityName"].ToString();
+                string dateConversation = dr["dateConversation"].ToString();
+                string duration = dr["duration"].ToString() + " min";
+                Console.WriteLine("{0,-35} {1,-15} {2,-10} {3,-20}", abonentSurName, cityName, duration, dateConversation);
+            }
+            dr.Close();
+            conn.Close();
+            Console.Read();
+        }
+    }
+ }
---
+++ phoneConversations/phoneCcnversations/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("phoneCcnversations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("phoneCcnversations")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8f0f0bb9-a6d7-4e9b-a5f8-d2d00b085517")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
